c2b9ae4b0a4e60bbe5d2368e18ec04c7dcdbb67b
menaruben <ruben@mena.ch>
2024-11-14 11:02:33 +0100

    added makefile and print_usage

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,21 @@ using namespace std;
 using namespace asa;
 using namespace transpiler;
 
+void print_usage() {
+  cout << "usage: asa [arg] source.asa" << endl;
+  cout << "  arg options:" << endl;
+  cout << "    run     interprets the source.asa file" << endl;
+  cout << "    build   compiles the source.asa file into an executable" << endl;
+}
+
 int main(int argc, char **argv) {
-  if (argc < 2)
-    throw runtime_error("Please enter a file path!");
-  string sourcepath = argv[1];
+  if (argc < 3) { 
+    print_usage();
+    return -1;
+  }
+  
+  string action = argv[1];
+  string sourcepath = argv[2];
   string source = read_file(sourcepath, "//");
   vector<Token> tokens = lexer::tokenize(source);
   Program program = load_program(tokens);
@@ -23,10 +34,19 @@ int main(int argc, char **argv) {
     cout << "[ERROR]: No entry point 'main' found!" << endl;
   }
 
-  Object result = interpreter::eval(program, "main", &stack);
-  if (result.error != Ok) {
-    cout << "[ERROR]: " << result.value << endl;
+  if (action == "run") {
+    Object result = interpreter::eval(program, "main", &stack);
+    if (result.error != Ok) {
+      cout << "[ERROR]: " << result.value << endl;
+      return -1;
+    }
+  } else if (action == "build") {
+    cout << "[ERROR]: Compilation not supported right now" << endl;
+    return -1;
+  } else {
+    print_usage();
     return -1;
   }
+  
   return 0;
 }
